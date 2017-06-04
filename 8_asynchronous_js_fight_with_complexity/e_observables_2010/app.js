b81b8ab8b180833927e7255e771e43b76715968b
Alexander Myshov <simplex.light@gmail.com>
2017-06-05 02:31:47 +0700

    Add example with observables

---
+++ 8_asynchronous_js_fight_with_complexity/e_observables_2010/app.js
@@ -0,0 +1,41 @@
+var Rx = require('rxjs');
+
+/**
+ * Emulate network connectivity
+ */
+function getDataObservable(url) {
+    var stubs = {
+        '/search?name=iPhone': 123,
+        '/shopOffers?id=123': ['AppleStore', 'DNS']
+    };
+
+    return Rx.Observable.create(function (observer) {
+        setTimeout(function () {
+            observer.next(stubs[url]);
+        }, 1000);
+    });
+} 
+
+
+searchModelWithObservables('iPhone');
+
+function searchModelWithObservables(modelName) {
+    getModelId(modelName)
+        .flatMap(getShops)
+        .map(printShops)
+        .subscribe();
+}
+
+function getModelId (modelName) {
+    return getDataObservable('/search?name=' + modelName);
+}
+
+function getShops(modelId) {
+    return getDataObservable('/shopOffers?id=' + modelId)
+}
+
+function printShops(shops) {
+    shops.forEach(function (shopName) {
+        console.log('Есть в магазине ' + shopName);
+    });
+}
